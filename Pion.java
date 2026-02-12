49b6080202b32420cfebfe68df1a9992e14ac988
Hakim Chentir <chentir.hakim2@gmail.com>
2026-02-12 11:40:53 +0100

    Seconde séance de TP

--- Pion.java
+++ Pion.java
@@ -1,8 +1,31 @@
-
 public class Pion {
 
-	private String nom;
+	private String couleur;
 	private int position;
-	private int nbcase = 30;
+	private int nbCase = 30;
 
+	public Pion(String couleur) {
+		this.couleur = couleur;
+		this.position = 1;
+	}
+
+	public void deplacer(int valeur_de) {
+		position += valeur_de;
+
+		if (position > nbCase) {
+			position = nbCase-(position-nbCase);
+		}
+	}
+
+	public int getPosition() {
+		return position;
+	}
+
+	public boolean aGagne() {
+		boolean etat = false;
+		if(position == nbCase) {
+			etat = true;
+		}
+		return etat;
+	}
 }
